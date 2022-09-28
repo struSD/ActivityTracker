fe91bf7cf072c94130b0a4896c4d43b5cf07e6a8
Danylo Strus <danylo.strus@gmail.com>
2022-09-28 14:37:49 +0300

    some fix

--- ActivityTracker.Api/Program.cs
+++ ActivityTracker.Api/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -16,10 +20,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
 
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
